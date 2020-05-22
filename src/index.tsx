9867870af8b90d209c4ac6e29962ef11ed9f447e
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-22 15:52:41 +0900

    set bubble to theme, set border

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'react-quill/dist/quill.snow.css'
+import 'react-quill/dist/quill.bubble.css'
 import ReactQuill from 'react-quill'
+import styled from 'styled-components'
 
 const Main = () => {
   const [value, setValue] = React.useState('')
 
+  React.useEffect(() => {
+    console.log(value)
+  }, [value])
+
   return (
-    <ReactQuill theme="snow" value={value} onChange={setValue} />
+    <Container>
+      <ReactQuill theme="bubble" value={value} onChange={setValue} />
+    </Container>
   )
 }
+
+const Container = styled.div`
+  border: 1px solid #ccc;
+`
 
 ReactDOM.render(<Main />, document.getElementById('root'))
